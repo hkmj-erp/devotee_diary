7e3986756ccfbbc0fb49fc74b95b77404f181095
hkmj-erp <it@harekrishnajaipur.org>
2024-05-29 16:39:58 +0530

    misc

--- devotee_diary/devotee_diary/doctype/ded_devotee/ded_devotee_tree.js
+++ devotee_diary/devotee_diary/doctype/ded_devotee/ded_devotee_tree.js
@@ -1,37 +1,38 @@
 frappe.treeview_settings["DED Devotee"] = {
-    breadcrumb: "Devotees",
-    get_tree_root: false,
-    root_label: "All Devotees",
-    get_tree_nodes: "devotee_diary.devotee_diary.doctype.ded_devotee.ded_devotee.get_children",
-    add_tree_node: "devotee_diary.devotee_diary.doctype.ded_devotee.ded_devotee.add_node",
-    ignore_fields: ["parent_ded_devotee"],
-    onload: function (treeview) {
-        treeview.make_tree();
+  breadcrumb: "Devotees",
+  get_tree_root: false,
+  root_label: "All Devotees",
+  get_tree_nodes:
+    "devotee_diary.devotee_diary.doctype.ded_devotee.ded_devotee.get_children",
+  add_tree_node:
+    "devotee_diary.devotee_diary.doctype.ded_devotee.ded_devotee.add_node",
+  ignore_fields: ["parent_ded_devotee"],
+  onload: function (treeview) {
+    treeview.make_tree();
+  },
+  fields: [
+    {
+      fieldtype: "Data",
+      fieldname: "initial",
+      label: __("Devotee Initial"),
+      reqd: true,
     },
-    fields: [
-        {
-            fieldtype: "Data",
-            fieldname: "initial",
-            label: __("Devotee Initial"),
-            reqd: true,
-        },
-        {
-            fieldtype: "Data",
-            fieldname: "full_name",
-            label: __("Devotee Full Name"),
-            reqd: true,
-        },
-        {
-            fieldtype: "Link",
-            options: "User",
-            fieldname: "erp_user",
-            label: __("ERP User Account"),
-            reqd: true,
-        },
-        {
-            fieldtype: "Check",
-            fieldname: "is_group",
-            label: __("Is DCS"),
-        },
-    ],
-}
+    {
+      fieldtype: "Data",
+      fieldname: "full_name",
+      label: __("Devotee Full Name"),
+      reqd: true,
+    },
+    {
+      fieldtype: "Link",
+      options: "User",
+      fieldname: "erp_user",
+      label: __("ERP User Account"),
+    },
+    {
+      fieldtype: "Check",
+      fieldname: "is_group",
+      label: __("Is DCS"),
+    },
+  ],
+};
--- devotee_diary/sadhana/utils.py
+++ devotee_diary/sadhana/utils.py
@@ -5,15 +5,18 @@ import frappe
 def get_parameters():
     parameters = []
     detail = frappe.qb.DocType("Sadhana Parameter Detail")
-    for parameter in frappe.get_all("Sadhana Parameter", fields=["*"], order_by = "priority"):
+    for parameter in frappe.get_all(
+        "Sadhana Parameter", fields=["*"], order_by="priority"
+    ):
         details = (
             frappe.qb.from_(detail)
-            .select("grade")
-            .orderby("grade")
+            .select("grade", "remarks", "points")
+            .orderby("idx")
             .where(detail.parent == parameter["name"])
             .run(as_dict=1)
         )
-        details = [d["grade"] for d in details]
-        parameter.update(options=details)
+        parameter.update(options=[d["grade"] for d in details])
+        parameter.update(remarks=[d["remarks"] for d in details])
+        parameter.update(points=[d["points"] for d in details])
         parameters.append(parameter)
     return parameters
